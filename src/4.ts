63b917238ca294fa6360a683a59c119315577077
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-10-30 22:12:36 +0100

    hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+class Key {
+	private signature: number;
+
+	constructor() {
+		this.signature = Math.random();
+	}
+	getSignature() {
+		return this.signature;
+	}
+}
+
+class Person {
+	private key: Key;
+
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	getKey() {
+		return this.key;
+	}
+}
+
+abstract class House {
+	protected door: boolean = false;
+	protected key: Key;
+	protected tenants: Person[] = [];
+
+	constructor(key: Key) {
+		this.key = key;
+	}
+
+	abstract openDoor(key: Key): void;
+
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person);
+			console.log("Person is in the house.");
+		} else {
+			console.log("Door is closed. Cannot enter.");
+		}
+	}
+}
+
+class MyHouse extends House {
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true;
+			console.log("Door is opened.");
+		} else {
+			console.log("Cannot open the door. Invalid key.");
+		}
+	}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +61,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
